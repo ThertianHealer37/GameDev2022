536c5654cf838b8dd1be38ef02517bb62ebb4162
ThertianHealer37 <marquezcarlos933@gmail.com>
2022-02-15 21:53:54 -0500

    Worked on Movement and Camera
    *Movement implementation.
    *Camera created needs work.

---
+++ GameDevSpring2022/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraMovementSpeed;
+    private Transform transform;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+     //   if (Input.)
+     //   transform.position += transform.right * (Time.deltaTime * 5);
+    }
+}
--- GameDevSpring2022/Assets/Scripts/Movement.cs
+++ GameDevSpring2022/Assets/Scripts/Movement.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody2D rb;
-    public Transform target;
-    public int velocity;
-    
+    private Rigidbody2D rb;
+    public float velocity;
+    public int jumpforce;
+    private float moveHorizontal;
+    private float moveVertical;
 
 
 
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        if(Input.GetKeyDown("Horizontal"))
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 14f);
+
+
+        moveHorizontal =Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
 
 
     }
 
-  
+    private void FixedUpdate()
+    {
+        if (moveHorizontal> 0.1f || moveHorizontal<0.1f)
+        {
+            rb.AddForce(new Vector2(velocity*moveHorizontal, 0), ForceMode2D.Impulse);
+        }
+
+        if (moveVertical >0.1f && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(new Vector2(0,jumpforce), ForceMode2D.Impulse);
+        }
+    }
+
 }
